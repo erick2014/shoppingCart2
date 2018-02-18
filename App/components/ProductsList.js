d718ad581f4df3009b8cf737a3c7eae9c4d11213
erick <erickarmandogarcia@hotmail.com>
2018-02-17 23:30:23 -0500

    Step4 Implement product list with images

--- App/components/ProductsList.js
+++ App/components/ProductsList.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, FlatList, Image } from 'react-native'
 
 class ProductsList extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: [
+				{ key: '1', url: 'http://onapples.com/uploads/varieties/4iw8z5071wzp.png', text: 'Apple' },
+				{ key: '2', url: 'https://vignette.wikia.nocookie.net/battlefordreamisland/images/a/ae/Bananas.png/revision/latest?cb=20170825134007', text: 'Bananas' },
+				{ key: '3', url: 'http://www.thienkhanh.vn/wp-content/uploads/2017/07/dua.png', text: 'Pinnaple' },
+			]
+		}
+	}
+
+	getItemList = photo => {
+		return (
+			<View style={styles.imgItem}>
+				<Image source={{ uri: photo.item.url }} style={styles.image} />
+			</View>
+		)
+	}
 
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Products list Screen</Text>
+				<Text style={styles.mainTitle}>
+					Fruits List
+        </Text>
+				<View style={styles.imagesList}>
+					<FlatList
+						data={this.state.products}
+						renderItem={this.getItemList}
+					/>
+				</View>
 			</View>
 		)
 	}
@@ -15,9 +42,25 @@ class ProductsList extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
+		justifyContent: 'flex-start',
 		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
+		backgroundColor: '#eaf0f3',
+	},
+	image: {
+		width: 150,
+		height: 150
+	},
+	imgItem: {
+		margin: 20,
+		backgroundColor: '#d9ebf5'
+	},
+	imagesList: {
+		height: 300,
+		marginBottom: 10
+	},
+	mainTitle: {
+		fontSize: 20,
+		marginTop: 40
 	}
 })
 
