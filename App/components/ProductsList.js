dda6125268458dd386192bb5423990c1ba60e592
erick <erickarmandogarcia@hotmail.com>
2018-02-17 23:34:09 -0500

    Fix code indentation in ProductsList file

--- App/components/ProductsList.js
+++ App/components/ProductsList.js
@@ -3,65 +3,65 @@ import { Text, View, StyleSheet, FlatList, Image } from 'react-native'
 
 class ProductsList extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			products: [
-				{ key: '1', url: 'http://onapples.com/uploads/varieties/4iw8z5071wzp.png', text: 'Apple' },
-				{ key: '2', url: 'https://vignette.wikia.nocookie.net/battlefordreamisland/images/a/ae/Bananas.png/revision/latest?cb=20170825134007', text: 'Bananas' },
-				{ key: '3', url: 'http://www.thienkhanh.vn/wp-content/uploads/2017/07/dua.png', text: 'Pinnaple' },
-			]
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [
+        { key: '1', url: 'http://onapples.com/uploads/varieties/4iw8z5071wzp.png', text: 'Apple' },
+        { key: '2', url: 'https://vignette.wikia.nocookie.net/battlefordreamisland/images/a/ae/Bananas.png/revision/latest?cb=20170825134007', text: 'Bananas' },
+        { key: '3', url: 'http://www.thienkhanh.vn/wp-content/uploads/2017/07/dua.png', text: 'Pinnaple' },
+      ]
+    }
+  }
 
-	getItemList = photo => {
-		return (
-			<View style={styles.imgItem}>
-				<Image source={{ uri: photo.item.url }} style={styles.image} />
-			</View>
-		)
-	}
+  getItemList = photo => {
+    return (
+      <View style={styles.imgItem}>
+        <Image source={{ uri: photo.item.url }} style={styles.image} />
+      </View>
+    )
+  }
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text style={styles.mainTitle}>
-					Fruits List
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.mainTitle}>
+          Fruits List
         </Text>
-				<View style={styles.imagesList}>
-					<FlatList
-						data={this.state.products}
-						renderItem={this.getItemList}
-					/>
-				</View>
-			</View>
-		)
-	}
+        <View style={styles.imagesList}>
+          <FlatList
+            data={this.state.products}
+            renderItem={this.getItemList}
+          />
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'flex-start',
-		alignItems: 'center',
-		backgroundColor: '#eaf0f3',
-	},
-	image: {
-		width: 150,
-		height: 150
-	},
-	imgItem: {
-		margin: 20,
-		backgroundColor: '#d9ebf5'
-	},
-	imagesList: {
-		height: 300,
-		marginBottom: 10
-	},
-	mainTitle: {
-		fontSize: 20,
-		marginTop: 40
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#eaf0f3',
+  },
+  image: {
+    width: 150,
+    height: 150
+  },
+  imgItem: {
+    margin: 20,
+    backgroundColor: '#d9ebf5'
+  },
+  imagesList: {
+    height: 300,
+    marginBottom: 10
+  },
+  mainTitle: {
+    fontSize: 20,
+    marginTop: 40
+  }
 })
 
 export default ProductsList
