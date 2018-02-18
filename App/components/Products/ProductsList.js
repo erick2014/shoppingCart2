0b0091ab7cdc3306e0e7b47f811ef0deff1282c2
erick <erickarmandogarcia@hotmail.com>
2018-02-18 18:35:05 -0500

    Implement product detail page

--- App/components/Products/ProductsList.js
+++ App/components/Products/ProductsList.js
@@ -5,101 +5,116 @@ import CustomCirle from './CustomCircle/CustomCircle'
 
 class ProductsList extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [
-        { key: '1', url: 'http://onapples.com/uploads/varieties/4iw8z5071wzp.png', text: 'Apple' },
-        { key: '2', url: 'https://vignette.wikia.nocookie.net/battlefordreamisland/images/a/ae/Bananas.png/revision/latest?cb=20170825134007', text: 'Bananas' },
-        { key: '3', url: 'http://www.thienkhanh.vn/wp-content/uploads/2017/07/dua.png', text: 'Pinnaple' },
-      ]
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: [
+				{
+					key: '1', url: 'http://onapples.com/uploads/varieties/4iw8z5071wzp.png',
+					text: 'Apple',
+					description: 'Apples are good for your body, also are delicious'
+				},
+				{
+					key: '2',
+					url: 'https://vignette.wikia.nocookie.net/battlefordreamisland/images/a/ae/Bananas.png/revision/latest?cb=20170825134007',
+					text: 'Bananas',
+					description: 'Bananas are for monkeys...'
+				},
+				{
+					key: '3',
+					url: 'http://www.thienkhanh.vn/wp-content/uploads/2017/07/dua.png',
+					text: 'Pinnaple',
+					description: 'Pinnable is yellow, is pretty, and delicious'
+				},
+			]
+		}
+	}
 
-  onClickEyeIcon = () => {
-    console.log('eye was clicked')
-  }
+	onClickEyeIcon = clickedProduct => {
+		const { navigation: { navigate } } = this.props;
+		navigate('ProductDetail', { 'product': clickedProduct['item'] })
+	}
 
-  getItemList = photo => {
-    return (
-      <View style={styles.itemList}>
-        <View style={styles.itemListLeftColumn}>
-          <Image source={{ uri: photo.item.url }} style={styles.itemListImage} />
-        </View>
-        <View style={styles.itemListRightColumn}>
+	getItemList = photo => {
+		return (
+			<View style={styles.itemList}>
+				<View style={styles.itemListLeftColumn}>
+					<Image source={{ uri: photo.item.url }} style={styles.itemListImage} />
+				</View>
+				<View style={styles.itemListRightColumn}>
 
-          <View style={styles.iconWrapper}>
-            <CustomCirle iconName={'add'} onClickHandler={() => { }} />
-          </View>
+					<View style={styles.iconWrapper}>
+						<CustomCirle iconName={'add'} onClickHandler={() => { }} />
+					</View>
 
-          <View style={styles.iconWrapper}>
-            <CustomCirle iconName={'eye'} onClickHandler={this.onClickEyeIcon} />
-          </View>
+					<View style={styles.iconWrapper}>
+						<CustomCirle iconName={'eye'} onClickHandler={() => { this.onClickEyeIcon(photo) }} />
+					</View>
 
-          <View style={styles.iconWrapper}>
-            <CustomCirle iconName={'shopping'} onClickHandler={() => { }} />
-          </View>
+					<View style={styles.iconWrapper}>
+						<CustomCirle iconName={'shopping'} onClickHandler={() => { }} />
+					</View>
 
-        </View>
-      </View >
-    )
-  }
+				</View>
+			</View >
+		)
+	}
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.mainTitle}>
-          Fruits List
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.mainTitle}>
+					Fruits List
 				</Text>
-        <View style={styles.imagesList}>
-          <FlatList
-            data={this.state.products}
-            renderItem={this.getItemList}
-          />
-        </View>
-      </View>
-    )
-  }
+				<View style={styles.imagesList}>
+					<FlatList
+						data={this.state.products}
+						renderItem={this.getItemList}
+					/>
+				</View>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#eaf0f3',
-  },
-  itemList: {
-    flex: 1,
-    flexDirection: 'row',
-    marginBottom: 10
-  },
-  itemListImage: {
-    width: 150,
-    height: 150
-  },
-  imagesList: {
-    height: 400,
-    marginBottom: 10
-  },
-  mainTitle: {
-    fontSize: 20,
-    marginTop: 20,
-    marginBottom: 20
-  },
-  iconWrapper: {
-    width: 50,
-    height: 50
-  },
-  itemListRightColumn: {
-    width: 50,
-    height: 150
-  },
-  itemListLeftColumn: {
-    width: 150,
-    height: 150,
-    backgroundColor: 'powderblue'
-  }
+	container: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		backgroundColor: '#eaf0f3',
+	},
+	itemList: {
+		flex: 1,
+		flexDirection: 'row',
+		marginBottom: 10
+	},
+	itemListImage: {
+		width: 150,
+		height: 150
+	},
+	imagesList: {
+		height: 400,
+		marginBottom: 10
+	},
+	mainTitle: {
+		fontSize: 20,
+		marginTop: 20,
+		marginBottom: 20
+	},
+	iconWrapper: {
+		width: 50,
+		height: 50
+	},
+	itemListRightColumn: {
+		width: 50,
+		height: 150
+	},
+	itemListLeftColumn: {
+		width: 150,
+		height: 150,
+		backgroundColor: 'powderblue'
+	}
 })
 
 export default ProductsList
--- App/reactNavigation/appNavigation.js
+++ App/reactNavigation/appNavigation.js
@@ -1,11 +1,31 @@
-import { TabNavigator } from 'react-navigation'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 
+//Products components
 import ProductsList from '../components/Products/ProductsList'
+import ProductDetail from '../components/Products/ProductDetail/ProductDetail'
+//Shopping cart component
 import ShoppingCart from '../components/ShoppingCart'
 
-const AppNavigation = TabNavigator({
-	Products: { screen: ProductsList },
-	Cart: { screen: ShoppingCart }
+const productsStack = StackNavigator({
+	Products: {
+		screen: ProductsList,
+		navigationOptions: ({ navigation }) => ({
+			header: null,
+		})
+	},
+	ProductDetail: {
+		screen: ProductDetail,
+		navigationOptions: ({ navigation }) => ({
+			header: null,
+		})
+	},
 })
+
+const AppNavigation = TabNavigator(
+	{
+		Products: { screen: productsStack },
+		Cart: { screen: ShoppingCart }
+	}
+)
 
 export default AppNavigation
