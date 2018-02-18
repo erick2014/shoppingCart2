2a53f58d39b44df3467e8456cedde40c81ae2fb0
erick <erickarmandogarcia@hotmail.com>
2018-02-18 18:43:21 -0500

    Remove console.log nd fix identation in ProductDetail comp

--- App/components/Products/ProductDetail/ProductDetail.js
+++ App/components/Products/ProductDetail/ProductDetail.js
@@ -2,34 +2,34 @@ import React from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native'
 
 const ProductDetail = props => {
-	const { navigation: { state: { params: { product } } } } = props
-	console.log('params ', product.url)
-	return (
-		<View style={styles.container}>
-			<View>
-				<Text style={styles.productTitle}> {product.text} </Text>
-			</View>
-			<View>
-				<Image source={{ uri: product.url }} style={styles.productImage} />
-			</View>
-			<View style={styles.productDescription}>
-				<Text>{product.description}</Text>
-			</View>
-		</View>
-	)
+  const { navigation: { state: { params: { product } } } } = props
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.productTitle}> {product.text} </Text>
+      </View>
+      <View>
+        <Image source={{ uri: product.url }} style={styles.productImage} />
+      </View>
+      <View style={styles.productDescription}>
+        <Text>{product.description}</Text>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'flex-start',
-		alignItems: 'center',
-		backgroundColor: '#eaf0f3',
-		marginTop: 40
-	},
-	productTitle: { fontSize: 22 },
-	productImage: { width: 150, height: 150 },
-	productDescription: { marginTop: 20 },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#eaf0f3',
+    marginTop: 40
+  },
+  productTitle: { fontSize: 22 },
+  productImage: { width: 150, height: 150 },
+  productDescription: { marginTop: 20 },
 })
 
 export default ProductDetail
