67eb03c3c52aaee960aa3376b4d23e5f9cd6c594
erick <erickarmandogarcia@hotmail.com>
2018-02-21 17:33:03 -0500

    change indentation in appWithStore.js file

--- App/reactNavigation/appWithStore.js
+++ App/reactNavigation/appWithStore.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 
 import {
-	addNavigationHelpers,
+  addNavigationHelpers,
 } from 'react-navigation'
 
 import {
-	createStore,
-	applyMiddleware,
-	combineReducers,
+  createStore,
+  applyMiddleware,
+  combineReducers,
 } from 'redux'
 
 import {
-	createReduxBoundAddListener,
-	createReactNavigationReduxMiddleware,
+  createReduxBoundAddListener,
+  createReactNavigationReduxMiddleware,
 } from 'react-navigation-redux-helpers'
 
 import { Provider, connect } from 'react-redux'
@@ -24,50 +24,50 @@ import appReducer from '../redux/reducers'
 // this is needed so that any events that mutate the navigation state
 // properly trigger the event listeners
 const middleware = createReactNavigationReduxMiddleware(
-	"root",
-	state => state.nav,
+  "root",
+  state => state.nav,
 )
 /*
-	Subscribe to updates to navigation lifecycle
-	willBlur - the screen will be unfocused
-	willFocus - the screen will focus
-	didFocus - the screen focused (if there was a transition, the transition completed)
-	didBlur - the screen unfocused (if there was a transition, the transition completed)
-	https://github.com/react-navigation/react-navigation/blob/master/examples/NavigationPlayground/js/SimpleStack.js#L119
+  Subscribe to updates to navigation lifecycle
+  willBlur - the screen will be unfocused
+  willFocus - the screen will focus
+  didFocus - the screen focused (if there was a transition, the transition completed)
+  didBlur - the screen unfocused (if there was a transition, the transition completed)
+  https://github.com/react-navigation/react-navigation/blob/master/examples/NavigationPlayground/js/SimpleStack.js#L119
 */
 const addListener = createReduxBoundAddListener("root")
 
 class App extends Component {
-	render() {
-		return (
-			<AppNavigator navigation={
-				addNavigationHelpers({
-					dispatch: this.props.dispatch,
-					state: this.props.nav,
-					addListener,
-				})
-			} />
-		);
-	}
+  render() {
+    return (
+      <AppNavigator navigation={
+        addNavigationHelpers({
+          dispatch: this.props.dispatch,
+          state: this.props.nav,
+          addListener,
+        })
+      } />
+    );
+  }
 }
 
 const AppWithNavigationState = connect(
-	state => ({ nav: state.nav })
+  state => ({ nav: state.nav })
 )(App)
 
 const store = createStore(
-	appReducer,
-	applyMiddleware(middleware),
+  appReducer,
+  applyMiddleware(middleware),
 )
 
 class RootApp extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<AppWithNavigationState />
-			</Provider>
-		);
-	}
+  render() {
+    return (
+      <Provider store={store}>
+        <AppWithNavigationState />
+      </Provider>
+    );
+  }
 }
 
 export default RootApp
