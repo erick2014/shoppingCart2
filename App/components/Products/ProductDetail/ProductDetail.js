6328de21a6792ba15225a621f4b5a9147c9f1d20
erick <erickarmandogarcia@hotmail.com>
2018-02-21 14:42:18 -0500

    <ProductDetail>Add a go to product list btn

--- App/components/Products/ProductDetail/ProductDetail.js
+++ App/components/Products/ProductDetail/ProductDetail.js
@@ -1,35 +1,41 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native'
+import { View, StyleSheet, Text, Image, Button } from 'react-native'
 
 const ProductDetail = props => {
-  const { navigation: { state: { params: { product } } } } = props
+	const { navigation: { navigate, state: { params: { product } }, } } = props
 
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.productTitle}> {product.text} </Text>
-      </View>
-      <View>
-        <Image source={{ uri: product.url }} style={styles.productImage} />
-      </View>
-      <View style={styles.productDescription}>
-        <Text>{product.description}</Text>
-      </View>
-    </View>
-  )
+	return (
+		<View style={styles.container}>
+			<View>
+				<Text style={styles.productTitle}> {product.text} </Text>
+			</View>
+			<View>
+				<Image source={{ uri: product.url }} style={styles.productImage} />
+			</View>
+			<View style={styles.productDescription}>
+				<Text>{product.description}</Text>
+			</View>
+			<View>
+				<Button
+					onPress={() => { navigate('Products') }}
+					title="Go to Products"
+				/>
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#eaf0f3',
-    marginTop: 40
-  },
-  productTitle: { fontSize: 22 },
-  productImage: { width: 150, height: 150 },
-  productDescription: { marginTop: 20 },
+	container: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		backgroundColor: '#eaf0f3',
+		marginTop: 40
+	},
+	productTitle: { fontSize: 22 },
+	productImage: { width: 150, height: 150 },
+	productDescription: { marginTop: 20 },
 })
 
 export default ProductDetail
