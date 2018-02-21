709ae973f302fa574cbc32c2807a4709ad258447
erick <erickarmandogarcia@hotmail.com>
2018-02-21 17:31:14 -0500

    Add comments as explanation in appWithStore.js

--- App/reactNavigation/appWithStore.js
+++ App/reactNavigation/appWithStore.js
@@ -21,11 +21,20 @@ import AppNavigator from './appNavigation'
 import appReducer from '../redux/reducers'
 
 // Note: must be run before createReduxBoundAddListener
+// this is needed so that any events that mutate the navigation state
+// properly trigger the event listeners
 const middleware = createReactNavigationReduxMiddleware(
 	"root",
 	state => state.nav,
 )
-
+/*
+	Subscribe to updates to navigation lifecycle
+	willBlur - the screen will be unfocused
+	willFocus - the screen will focus
+	didFocus - the screen focused (if there was a transition, the transition completed)
+	didBlur - the screen unfocused (if there was a transition, the transition completed)
+	https://github.com/react-navigation/react-navigation/blob/master/examples/NavigationPlayground/js/SimpleStack.js#L119
+*/
 const addListener = createReduxBoundAddListener("root")
 
 class App extends Component {
